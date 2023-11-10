613b1c1b5e7b12252645fd09154b08c63abc41f7
Skr299 <S567427@nwmissouri.edu>
2023-11-10 02:02:58 -0600

    made changes

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -77,3 +77,24 @@ exports.restaurants_detail = async function(req, res) {
  res.send(`{"error": document for id ${req.params.id} not found`);
  }
 };
+
+// Handle restaurant update form on PUT.
+exports.restaurants_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body 
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await restaurants.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.restaurants_type) 
+    toUpdate.restaurants_type = req.body.restaurants_type;
+    if(req.body.address) toUpdate.address = req.body.address;
+    if(req.body.rating) toUpdate.rating = req.body.rating;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id} 
+   failed`);
+    }
+   };
